d94eada8e5d23474948b1b501a76e4710fed5371
Arun Roy <arunroy0065@gmail.com>
2026-01-12 11:52:57 +0600

    refactor: fabric 6.9.0 version installed'

--- src/lib/services/createDataURL.js
+++
@@ -1,8 +0,0 @@
-export const createDataURL = (editorRef) => {
-    const dataURL = editorRef.toDataURL({
-        format: 'png',
-        quality: 1,
-        multiplier: 1,
-    });
-    return dataURL
-}
