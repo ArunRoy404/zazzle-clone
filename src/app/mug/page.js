6bb629aebdb38012f7ba972525c94c10aafdbf32
Arun Roy <arunroy0065@gmail.com>
2026-01-13 09:58:39 +0600

    refactor: component renamed to RenderModel.jsx

--- src/app/mug/page.js
+++ src/app/mug/page.js
@@ -4,10 +4,10 @@ import MemoryOptions from '@/components/EditorComponents/MemoryOptions';
 import BodyEditor from '../components/BodyEditor';
 import TextOptionsHorizontal from '../components/TextOptions/TextOptionsHorizontal';
 import RenderImage from '@/components/RenderImage/RenderImage';
-import RenderModel from '@/components/ThreeJsExample/ThreeJSExample';
 import { modelList } from '@/data/modelList';
 import useModelStore from '@/store/useModelStore';
 import { useEffect } from 'react';
+import RenderModel from '@/components/ThreeJsExample/ThreeJSExample';
 
 
 
@@ -25,7 +25,7 @@ const ThreeJsEditor = () => {
   return (
     <div className='relative bg-gray-50 flex-1 flex items-center justify-center h-full'>
       <div className='fixed top-20 right-10 z-100'>
-        <renderModel />
+        <RenderModel />
       </div>
 
       <div className='fixed top-80 right-10 z-10'>
