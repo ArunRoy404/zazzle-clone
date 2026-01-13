5b5bce8f220e201375d5955d5075e2eaf1e3611e
Arun Roy <arunroy0065@gmail.com>
2026-01-13 16:26:30 +0600

    feat: some issue fixed

--- src/components/EditorComponents/Editor/EditorTextInsert.jsx
+++ src/components/EditorComponents/Editor/EditorTextInsert.jsx
@@ -38,34 +38,6 @@ const EditorTextInsert = ({ isMobile = false }) => {
             >
                 T   Add a text box
             </Button>
-
-            {/* <p className='py-4'>Default text styles</p>
-
-            <div
-                className='flex flex-col gap-2'
-            >
-                <Button
-                    onClick={() => handleAddText({ text: 'Heading', fontSize: 52, fontWeight: 'bold' })}
-                    variant='outline'
-                    className='border-border text-3xl! font-bold p-2 justify-start hover:scale-100 active:scale-100'
-                >
-                    Add a heading
-                </Button>
-                <Button
-                    onClick={() => handleAddText({ text: 'Subheading', fontSize: 32 })}
-                    variant='outline'
-                    className='border-border text-normal! p-2 justify-start hover:scale-100 active:scale-100'
-                >
-                    Add a subheading
-                </Button>
-                <Button
-                    onClick={() => handleAddText({ text: 'This is a body text', fontSize: 16, fontWeight: 'normal' })}
-                    variant='outline'
-                    className='border-border text-[12px]! p-2 justify-start hover:scale-100 active:scale-100'
-                >
-                    Add a little bit of body text
-                </Button>
-            </div> */}
         </DropdownMenuContent>
     );
 };
